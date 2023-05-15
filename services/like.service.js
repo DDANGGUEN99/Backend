5538595b8f684cc932f687fd0e06f9f1cb23678a
SUMMER-DESKTOP\jungh <junghs710@naver.com>
2023-05-15 13:06:51 +0900

    [fix] like error

--- services/like.service.js
+++ services/like.service.js
@@ -1,29 +1,34 @@
 const LikeRepository = require('../repositories/like.repository');
+const ItemRepository = require('../repositories/item.repository');
 const AppError = require('../utils/appError');
 
-class PostService {
+class ItemService {
   likeRepository = new LikeRepository();
-  postRepository = new PostRepository();
+  itemRepository = new ItemRepository();
 
-  putlike = async (post_id, user_id) => {
-    const post = await this.postRepository.findOne(post_id);
-    if (!post) {
+  putlike = async (item_id, user_id) => {
+    const item = await this.itemRepository.findOne(item_id);
+    if (!item) {
       throw new AppError(404, '판매글이 존재하지 않습니다.');
     }
 
-    const like = await this.likeRepository.findOne(post_id, user_id);
+    const like = await this.likeRepository.findOne(item_id, user_id);
     if (like) {
-      await this.likeRepository.destroy(post_id, user_id);
+      await this.likeRepository.destroy(item_id, user_id);
       return '내 관심목록에서 삭제되었습니다.';
     } else {
-      await this.likeRepository.create(post_id, user_id);
+      await this.likeRepository.create(item_id, user_id);
       return '내 관심목록에 등록되었습니다.';
     }
   };
 
-  getlikePost = async (user_id) => {
-    return await this.likeRepository.findlikePost(user_id);
+  getlikeItem = async (user_id) => {
+    try {
+      return await this.likeRepository.findlikeItem(user_id);
+    } catch (err) {
+      console.error(err);
+    }
   };
 }
 
-module.exports = PostService;
+module.exports = ItemService;
