0e64e3828a0d771a40e7fedec07d094e567fbd61
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-05-16 13:50:43 +0900

    [Fix] Javascript V2 to V3 conversion

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,10 +1,10 @@
-const aws = require('aws-sdk');
+const { S3 } = require('@aws-sdk/client-s3');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const path = require('path');
 
 require('dotenv').config();
-const s3 = new aws.S3({
+const s3 = new S3({
   region: process.env.AWS_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
