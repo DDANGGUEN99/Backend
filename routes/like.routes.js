f2c755102e9b8bbc589b154d63c0c539489147f9
corsair-hs <hsk.computer.1@gmail.com>
2023-05-18 00:18:44 +0900

    [Feat] 관심목록 추가/삭제 기능 추가

--- routes/like.routes.js
+++ routes/like.routes.js
@@ -5,7 +5,10 @@ const authMiddleware = require('../middlewares/auth-middleware');
 const LikesController = require('../controllers/like.controller');
 const likesController = new LikesController();
 
+// 관심목록 조회
 router.get('/like', authMiddleware, likesController.getlikeItem);
+
+// 관심목록 추가 / 삭제
 router.put('/:item_id/like', authMiddleware, likesController.putlike);
 
 module.exports = router;
