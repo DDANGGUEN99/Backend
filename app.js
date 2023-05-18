248d3e0d31568ac9f27464e916f9f942a5535f7b
DongHwna Lee <meadding231@gmail.com>
2023-05-18 17:54:56 +0900

    [fix] 판매글 전체 조회에서 판매글 전체 개수 반환하게 수정

--- app.js
+++ app.js
@@ -31,19 +31,33 @@ server.listen(port, () => {
   console.log(`running ${port}`);
 });
 
-// 방을 저장해 둘 자료구조가 있어야 할 것 같음.
-const ChatRooms = {};
+// socket을 저장해 둘 자료구조.
+const chatRooms = new Map();
 
-// 채팅이 들어온다는 것은 어떤 의미일까?
 io.on('connection', (socket) => {
   console.log('User connected' + socket.id);
 
-  socket.on('disconnect', () => {
+  socket.on('room_enter', (enterData) => {
+    const { room_id, user_id } = enterData;
+    if (chatRooms.has(room_id)) {
+      // room_id의 채팅방이 존재하는 경우
+      chatRooms.get(room_id).set(user_id, socket.id);
+    } else {
+      // room_id의 채팅방이 존재하지 않는 경우
+      const socketMap = new Map();
+      socketMap.set(user_id, socket.id);
+      chatRooms.set(room_id, socketMap);
+    }
+  });
+
+  socket.on('disconnect', (room_id, user_id) => {
     console.log('유저 나갔다');
+    // 방 자료구조에서 나가는 구조 짜기.
+    // 만약 다 나갔으면 방도 없애주면 좋을 것 같다.
+    chatRooms.get(room_id).delete(user_id);
   });
 
   socket.on('message', (message) => {
-    console.log(message);
     appSocket.saveChat(message);
     io.emit('message', message);
   });
